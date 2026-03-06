01b87f2bc4f05689dde007d5bf6124b77bb006e6
georgecoulon <georgecoulon@gmail.com>
2026-03-06 20:29:26 +0000

    Create App.js

---
+++ react/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import VariableDisplay from './VariableDisplay.js';
+
+function App() {
+  return (
+    <div className="variables">
+      <VariableDisplay />
+    </div>
+  );
+}
+
+export default App;

6335bd1d9d489a30a351ce13bf921ee0fbfdfbde
georgecoulon <georgecoulon@gmail.com>
2026-03-07 20:10:59 +0000

    Create App.js

---
+++ react/react_2_job_board/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JobBoard from './JobBoard.js';
+
+function App() {
+  return (
+    <div className="variables">
+      <JobBoard />
+    </div>
+  );
+}
+
+export default App;

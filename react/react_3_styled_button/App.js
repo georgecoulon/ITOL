022eae37190d0ad5235eef6fcaf5dbee4f6eb89f
georgecoulon <georgecoulon@gmail.com>
2026-03-08 19:26:57 +0000

    Create App.js

---
+++ react/react_3_styled_button/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StyledButton from './StyledButton.js';
+
+function App() {
+  return (
+    <div className="variables">
+      <StyledButton />
+    </div>
+  );
+}
+
+export default App;

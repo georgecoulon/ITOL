24c73750329026463af609ddd89820ab33b2449d
georgecoulon <georgecoulon@gmail.com>
2026-03-10 20:18:05 +0000

    Create App.js

---
+++ react/react_4_job_counter/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AdvancedJobCounter from './AdvancedJobCounter.js';
+
+function App() {
+  return (
+    <div className="variables">
+      <AdvancedJobCounter />
+    </div>
+  );
+}
+
+export default App;

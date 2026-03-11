bca8fe5cfe625d5d40de8e283fabdb1c86701b73
georgecoulon <georgecoulon@gmail.com>
2026-03-11 21:00:41 +0000

    Create App.js

---
+++ react/react_5_dynamic_form/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DynamicForm from './DynamicForm.js';
+
+function App() {
+  return (
+    <div className="variables">
+      <DynamicForm />
+    </div>
+  );
+}
+
+export default App;

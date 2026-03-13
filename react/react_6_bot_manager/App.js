f41f0ce901695d1bdff735c08059820314566f0d
georgecoulon <georgecoulon@gmail.com>
2026-03-13 21:19:32 +0000

    Create App.js

---
+++ react/react_6_bot_manager/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BotListManager from './BotListManager.js';
+
+function App() {
+  return (
+    <div className="variables">
+      <BotListManager />
+    </div>
+  );
+}
+
+export default App;

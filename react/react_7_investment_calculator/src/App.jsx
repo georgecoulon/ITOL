f0ce061e967387a19c59a40ce8292e0cb11f73aa
georgecoulon <georgecoulon@gmail.com>
2026-05-02 21:25:47 +0100

    Update App.jsx
    Added UserInput component

--- react/react_7_investment_calculator/src/App.jsx
+++ react/react_7_investment_calculator/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from './components/Header.jsx'
 import './App.css'
+import UserInput from './components/UserInput';
 
 function App() {
   
   return (
     <>
       <Header />
-      {/* additional components goes here */}
+      <UserInput />
     </>
   )
 }

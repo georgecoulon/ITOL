87b58444cd8a6223fb0b4bab7be120d67178ffce
georgecoulon <georgecoulon@gmail.com>
2026-05-03 19:36:37 +0100

    Update App.jsx
    Add OutputData

--- react/react_7_investment_calculator/src/App.jsx
+++ react/react_7_investment_calculator/src/App.jsx
@@ -1,13 +1,15 @@
 import Header from './components/Header.jsx'
 import './App.css'
 import UserInput from './components/UserInput';
+import OutputData from './components/OutputInput.jsx';
 
 function App() {
   
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onInputChange={handleChange} />
+      <OutputData inputValue={userInput} />
     </>
   )
 }

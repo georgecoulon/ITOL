177c0a6c1cc63a7712afce4c99e46e7c23ef2ecd
georgecoulon <georgecoulon@gmail.com>
2026-05-03 19:39:40 +0100

    Update App.jsx
    Corrected component name

--- react/react_7_investment_calculator/src/App.jsx
+++ react/react_7_investment_calculator/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header.jsx'
 import './App.css'
 import UserInput from './components/UserInput';
-import OutputData from './components/OutputInput.jsx';
+import OutputData from './components/OutputData.jsx';
 
 function App() {
   

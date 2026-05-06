a11bff5face0539f4291df1b32573e07ec2fb2fb
georgecoulon <georgecoulon@gmail.com>
2026-05-06 22:37:24 +0100

    Update App.jsx

--- react/react_7_investment_calculator/src/App.jsx
+++ react/react_7_investment_calculator/src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import Header from './components/Header.jsx'
 import './App.css'
-import UserInput from './components/UserInput';
+import UserInput from './components/UserInput.jsx';
 import OutputData from './components/OutputData.jsx';
 
 function App() {
-  
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [inputIdentifier]: +newValue
+    }));
+  };
+
   return (
     <>
       <Header />

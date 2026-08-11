cc8bdeedc93c66479fcc4780ccd3ad09ee456979
georgecoulon <georgecoulon@gmail.com>
2026-08-11 20:47:43 +0100

    Update Header.jsx

--- react/react_7_investment_calculator/src/components/Header.jsx
+++ react/react_7_investment_calculator/src/components/Header.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import logo from '../assets/logo.jpg'
 
 const Header = () => {
   return (
     <header id="header">
-      <img src={logo} alt="Investment Calculator Logo" />
       <h1>Investment Calculator</h1>
     </header>
   )

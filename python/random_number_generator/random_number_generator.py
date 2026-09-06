38d6bc038119fbfb7197885cc86c8418a50d7fc5
georgecoulon <georgecoulon@gmail.com>
2026-09-06 21:19:08 +0100

    Create random_number_generator.py

---
+++ python/random_number_generator/random_number_generator.py
@@ -0,0 +1,12 @@
+# Random Number Generator
+
+import random
+
+random_number = random.randint(1,100)
+
+print("Random number:", random_number)
+print("Date type:", type(random_number))
+
+print("Random number as float:", float(random_number))
+
+print("Random number as complex:", complex(random_number))

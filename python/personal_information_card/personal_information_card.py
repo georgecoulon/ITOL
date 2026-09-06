94e3c09acaaf5ccc67163d0b966c58410b63423f
georgecoulon <georgecoulon@gmail.com>
2026-09-06 19:55:42 +0100

    Create personal_information_card.py

---
+++ python/personal_information_card/personal_information_card.py
@@ -0,0 +1,11 @@
+# Personal Information Card
+
+name = "George"
+age = 36
+city = "Tunbridge Wells"
+hobby = "reading"
+
+print("My name is "+name+".")
+print("I am "+str(age)+" years old.")
+print("I live in "+city+".")
+print("I enjoy "+hobby+".")

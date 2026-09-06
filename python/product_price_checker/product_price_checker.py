b5eed29b5582a75982c0ddebfd0dd53ba5ded1f6
georgecoulon <georgecoulon@gmail.com>
2026-09-06 19:05:46 +0100

    Create product_price_checker.py

---
+++ python/product_price_checker/product_price_checker.py
@@ -0,0 +1,12 @@
+# Product Price Checker
+
+apples = 1.5
+pears = 1.25
+
+# check which product is more expensive and print out the least expensive one
+if apples > pears:
+    print("Pears is less expensive than apples")
+elif pears > apples:
+    print("Apples is less expensive than pears")
+else:
+    print("Both products have the same price")
